accf106fd6a14dc33a9302b10c1b6a2f4f432cb4
Danlolami <108428020+Danlolami@users.noreply.github.com>
2023-02-15 10:23:29 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function multiply(number1, number2) {
+    return number1 * number2;
+  }
+  console.log(multiply(50, 2));
+  //variables
+  let firstname = "Esther";
+  let lastname = "Oyalakun";
+  let stuterntrack = "Frontend developer"
+  let skills = "HTML, CSS, Sewing, Writing, e.t.c"
+  let favourite_color ="Blue";
+  let year_of_independence ="1960";
+  
+  console.log(firstname);
+  console.log(lastname);
+  console.log(stuterntrack);
+  console.log(skills);
+  console.log(favourite_color);
+  console.log(year_of_independence);
+  
+  console.log("My name is " + firstname  + " "+ lastname + ". I am a " + stuterntrack + ", my skills are :" + skills + ", my favourite color is " + favourite_color + ", my country gained independence in the year " + year_of_independence + "." );
+ let bestfood = "rice_and_beans"
+ console.log(bestfood)
